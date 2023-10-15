f94cc04a96990d6249ca4f25ba44ea0c8f6e27df
Rananda Dian Ilham <=>
2023-10-15 16:11:12 +0700

    add news page

--- src/app/(routes)/news/page.tsx
+++ src/app/(routes)/news/page.tsx
@@ -1,10 +1,38 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>news</div>
+    <div className='min-h-[74vh] my-20'>
+      <div className='news-wrapper flex flex-col w-2/4 mx-auto'>
+        <Link href={"/news/0"}>
+          <img
+            src={"/images/news/banner_0.jpeg"}
+            className='w-full'
+          />
+        </Link>
+        <Link href={"/news/1"}>
+          <img
+            src={"/images/news/banner_1.jpeg"}
+            className='w-full'
+          />
+        </Link>
+        <Link href={"/news/2"}>
+          <img
+            src={"/images/news/banner_2.jpeg"}
+            className='w-full'
+          />
+        </Link>
+        <Link href={"/news/3"}>
+          <img
+            src={"/images/news/banner_3.jpeg"}
+            className='w-full'
+          />
+        </Link>
+      </div>
+    </div>
   )
 }
 
