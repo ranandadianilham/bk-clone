e04c01f554bf52334b6a2ebbca310196a85097cf
Rananda Dian Ilham <=>
2023-10-16 09:41:51 +0700

    add data

--- src/app/_datas/menu.js
+++ src/app/_datas/menu.js
@@ -42,3 +42,67 @@ export const menuGroup = [
         image: ''
     },
 ]
+
+export const drinkOption = [
+    {
+        "id": "drink_0",
+        "title": "Double Choco Pie",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/ghq3xolsqea9bxfrtkatew_drink_selection.png"
+    },
+    {
+        "id": "drink_1",
+        "title": "King fusion Cookies and Cream",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/h8tcatpvzjca5ba3dqkapn_drink_selection.png"
+    },
+    {
+        "id": "drink_2",
+        "title": "Super Float Fanta",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/jggd5khurtgxctc5mbnepn_drink_selection.png"
+    },
+    {
+        "id": "drink_3",
+        "title": "Super Float Coke",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/eegeary4zq6av9yymeqgtm_drink_selection.png"
+    },
+    {
+        "id": "drink_4",
+        "title": "Milo Cookies & Cream",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/edmsf4d28fmmeo3kknmu2f_drink_selection.png"
+    },
+    {
+        "id": "drink_5",
+        "title": "Kopi Gula Aren",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/8yoasyta3ob26fjghbb5qw_drink_selection.png"
+    },
+    {
+        "id": "drink_6",
+        "title": "Chocolate Sundae",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/xynj3brbphm8nwcwehyuj8_drink_selection.png"
+    },
+    {
+        "id": "drink_7",
+        "title": "Super Float Milo",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/apbsxptmqrr7sg3a5sdmvf_drink_selection.png"
+    },
+    {
+        "id": "drink_8",
+        "title": "Super Float Mango",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/socckrbhmxghjsin5uu5oc_drink_selection.png"
+    },
+    {
+        "id": "drink_9",
+        "title": "Super Float Mocha",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/nhk9tgn9s3qipbmin3nu3r_drink_selection.png"
+    },
+    {
+        "id": "drink_10",
+        "title": "Milo Dinosaur",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/sa5z5xeygyvp9ralnjqgqy_drink_selection.png"
+    },
+    {
+        "id": "drink_11",
+        "title": "Vanilla Sundae",
+        "image": "https://media-order.bkdelivery.co.id/thumb/modifier_photo/2023/3/29/tj3gvw45fbrhgfabwt77at_drink_selection.png"
+    }
+]
+
